5d988c03ba8c4bdc41a83dc789a92b52ed660993
IanCapetillo <88984729+IanCapetillo@users.noreply.github.com>
2021-08-21 16:41:19 -0500

    Update exercise.py

--- assignments/08Caracol/src/exercise.py
+++ assignments/08Caracol/src/exercise.py
@@ -3,11 +3,13 @@ def main():
     min=0
     cm=0
     seg=0
+    mm=0
     
     min=float(input())
     seg=min*60
     
-    cm=seg*5.7
+    mm=(seg*5.7)
+    cm=(mm/10)
     
     print(cm)
     
