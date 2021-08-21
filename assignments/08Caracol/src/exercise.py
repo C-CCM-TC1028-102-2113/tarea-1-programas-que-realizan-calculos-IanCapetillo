01b32d289cede9dc64b10c8b6cfba184d4db8d3b
IanCapetillo <88984729+IanCapetillo@users.noreply.github.com>
2021-08-21 16:49:47 -0500

    Update exercise.py

--- assignments/08Caracol/src/exercise.py
+++ assignments/08Caracol/src/exercise.py
@@ -1,13 +1,10 @@
 def main():
     #escribe tu código abajo de esta línea
-    min=0
-    cm=0
-    
     min=float(input())
-    
-    cm=(min*342)
-    print(cm)
-    
+
+cm=(min*342)/10
+
+print(cm)
     pass
 
 if __name__ == '__main__':
