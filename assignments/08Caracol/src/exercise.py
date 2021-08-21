da9789d7016ef66f19a0c49fe36a602a1dfd627d
IanCapetillo <88984729+IanCapetillo@users.noreply.github.com>
2021-08-21 16:20:35 -0500

    Update exercise.py

--- assignments/08Caracol/src/exercise.py
+++ assignments/08Caracol/src/exercise.py
@@ -1,5 +1,16 @@
 def main():
     #escribe tu código abajo de esta línea
+    min=0
+    cm=0
+    seg=0
+    
+    min=float(input())
+    seg=min*60
+    
+    cm=seg*5.7
+    
+    print(cm)
+    
     pass
 
 if __name__ == '__main__':
